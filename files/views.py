49e3e7f0ed98766c33b83410b7ab0aa25fcf4fdb
quasimuzzle <anna.akhmyatzanova@gmail.com>
2024-01-12 03:51:48 +0400

    edit

--- files/views.py
+++ files/views.py
@@ -50,12 +50,12 @@ def add_rating(request, file_id):
         return redirect('file_detail', file_id=file_id)
     
 
-# def upload_file(request):
-#     if request.method == 'POST':
-#         form = FileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             file = form.save()
-#             return redirect('file_detail', file_id=file.pk)
-#     else:
-#         form = FileForm()
-#     return render(request, 'files/upload_file.html', {'form': form})  
+def upload_file(request):
+     if request.method == 'POST':
+         form = FileForm(request.POST, request.FILES)
+         if form.is_valid():
+             file = form.save()
+             return redirect('file_detail', file_id=file.pk)
+     else:
+         form = FileForm()
+     return render(request, 'files/upload_file.html', {'form': form})  
